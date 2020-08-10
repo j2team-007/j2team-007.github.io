065079a2c3eca59ca103bf3bc21808ad90488864
rikkapro <boyhaykhocnhoe@gmail.com>
2020-08-10 10:13:25 +0700

    update site property courses

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,36 @@
 const Course = require('../model/Course.js');
 const { mutipleMongooseObject } = require('../help/tool.js');
+const { MongooseObject } = require('../help/tool.js');
 
 class SiteController {
-    // GET [/home/]
-    index(req, res, next) {
+    // GET [/home/courses]
+    courses(req, res, next) {
         //     res.render('home');
         Course.find({})
             .then((Course) => {
-                res.render('home', {
+                res.render('courses', {
                     Course: mutipleMongooseObject(Course),
                 });
             })
             .catch(next);
     }
+
+    // GET [/home/:slug]
+    slug(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then((Course) => {
+                res.render('courses-propety', {
+                    Course: MongooseObject(Course),
+                });
+            })
+            .catch(next);
+    }
+
+    // GET [/home/]
+    index(req, res) {
+        res.render('home');
+    }
+
     // GET [/about/]
     about(req, res) {
         res.render('about');
--- src/routes/site.js
+++ src/routes/site.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const siteController = require('../app/controllers/SiteController.js');
 
+router.get('/courses', siteController.courses);
+
+router.get('/courses/:slug', siteController.slug);
+
 router.get('/about', siteController.about);
 
 router.get('/', siteController.index);
